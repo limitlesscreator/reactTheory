b8b53bd38e2b62f9b8560ce8ed5a0ffc595dbf5b
Vladislav V <0428vlad0428@mail.ru>
2021-08-22 09:17:31 +0300

    #1 Redux JS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
+//
+// import {App} from "./App";
+//
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
 
-import {App} from "./App";
+import {createStore} from "redux";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+function reducer(state = [], action) {
+    // console.log(action)
+    switch (action.type) {
+        case "ADD_TRACK":
+            return [
+                ...state,
+                action.payload
+            ]
+    }
+    return state
+}
+
+const store = createStore(reducer)
+
+console.log(store.getState())
+
+store.subscribe(() => {
+    console.log('state changed', store.getState())
+})
+
+store.dispatch({type: "ADD_TRACK", payload: "Smells like spirit"})
+store.dispatch({type: "ADD_TRACK", payload: "Enter Sandman"})
